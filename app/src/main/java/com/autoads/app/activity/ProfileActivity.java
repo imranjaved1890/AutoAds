18343f9b07313410221ba35f9fa10127eca1f23b
unknown <Asad Android>
2019-04-11 21:56:16 +0500

    all commits

--- app/src/main/java/com/autoads/app/activity/ProfileActivity.java
+++ app/src/main/java/com/autoads/app/activity/ProfileActivity.java
@@ -42,6 +42,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.logout_btn:
+                FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
